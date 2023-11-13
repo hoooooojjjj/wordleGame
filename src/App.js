bcac0a2b9cd3f6be34e853b76d080d4a8b81e88f
hoooooojjjj <rhj080471@gmail.com>
2023-11-14 03:00:06 +0900

    Words API에서 5글자인 영단어 가져와서 랜덤하게 뽑기 테스트 성공

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import API_KEY from "./API";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // 5글자인 영단어 모음 state
+  const [fiveLetterWords, setFiveLetterWords] = useState([]);
+  // 랜덤하게 뽑은 정답 영단어
+  const [randomWord, setRandomWord] = useState("");
+
+  // Words API에서 5글자인 영단어 가져오기
+  const getData = async () => {
+    const url =
+      "https://wordsapiv1.p.rapidapi.com/words/?letters=5&limit=10669";
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": API_KEY,
+        "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      const TrueData = JSON.parse(result).results.data.slice(
+        200,
+        JSON.parse(result).results.data.length
+      );
+      setFiveLetterWords(TrueData || []);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // getData() 호출
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // 랜덤하게 정답 단어 하나 뽑기
+  useEffect(() => {
+    if (fiveLetterWords.length > 0) {
+      const randomIndex = Math.floor(Math.random() * fiveLetterWords.length);
+      setRandomWord(fiveLetterWords[randomIndex]);
+    }
+  }, [fiveLetterWords]);
+
+  return <div className="App">{randomWord}</div>;
 }
 
 export default App;
